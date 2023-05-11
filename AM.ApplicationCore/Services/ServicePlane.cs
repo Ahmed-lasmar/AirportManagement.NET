d4436b898af5dd81fbf3be05f300fbe83582a03b
Ahmed Lasmar <ahmed.lasmar@esprit.tn>
2023-05-11 09:46:18 +0100

    Add project files.

---
+++ AM.ApplicationCore/Services/ServicePlane.cs
@@ -0,0 +1,20 @@
+﻿using AM.ApplicationCore.Interfaces;
+using AM.ApplicationCore.Domain;
+using System.Collections.Generic;
+
+namespace AM.ApplicationCore.Services
+{
+    public class ServicePlane : Service<Plane>, IServicePlane
+    {
+        
+        public ServicePlane(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+            
+        }
+        
+
+       
+
+       
+    }
+}
